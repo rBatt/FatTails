146590ed2f0c9e382ea14582e6cdb2955b711817
rbatt <battrd@gmail.com>
2014-10-01 08:58:27 -0400

    added a plot of xi v lambda

--- dscat_xiTS_xiRES_plot.R
+++ dscat_xiTS_xiRES_plot.R
@@ -22,10 +22,31 @@ source("/Users/Battrd/Documents/School&Work/WiscResearch/FatTails/fatPlot_Functi
 # ===================================
 png("/Users/Battrd/Documents/School&Work/WiscResearch/FatTails/Figures/dscat_xiTS_xiRES.png", res=150, units="in", height=3.5, width=3.5)
 # dev.new(height=3.5, width=3.5)
-dscat(z[,"xi.resid"], z[,"xi"], mar=c(2.5,2.5,0,0), cex=1, ps=9, family="Times", mgp=c(3, 0.5, 0), tcl=-0.35)
+dscat(z[,"xi.resid"], z[,"xi2"], mar=c(2.5,2.5,0,0), cex=1, ps=9, family="Times", mgp=c(3, 0.5, 0), tcl=-0.35)
 # abline(lm(xi~xi.resid, data=z[,]), lty="dashed", lwd=1)
 mtext(bquote(xi[Time~Series]), side=2, line=1.35, cex=1)
 mtext(bquote(xi[ARMA~Residuals]), side=1, line=1.5)
 dev.off()
+
+
+png("/Users/Battrd/Documents/School&Work/WiscResearch/FatTails/Figures/dscat_xiTS_lambda.png", res=150, units="in", height=3.5, width=3.5)
+# dev.new(height=3.5, width=3.5)
+dscat(z[,"lambda"], z[,"xi2"], mar=c(2.5,2.5,0,0), cex=1, ps=9, family="Times", mgp=c(3, 0.5, 0), tcl=-0.35)
+# abline(lm(xi~xi.resid, data=z[,]), lty="dashed", lwd=1)
+mtext(bquote(xi[Time~Series]), side=2, line=1.35, cex=1)
+mtext(bquote(abs(abs({lambda}))), side=1, line=1.5)
+dev.off()
+
+
+
+png("/Users/Battrd/Documents/School&Work/WiscResearch/FatTails/Figures/dscat_xiTS_lambda.png", res=150, units="in", height=3.5, width=3.5)
+# dev.new(height=3.5, width=3.5)
+dscat(z[,"p"]+z[,"q"], z[,"xi2"], mar=c(2.5,2.5,0,0), cex=1, ps=9, family="Times", mgp=c(3, 0.5, 0), tcl=-0.35)
+# abline(lm(xi~xi.resid, data=z[,]), lty="dashed", lwd=1)
+mtext(bquote(xi[Time~Series]), side=2, line=1.35, cex=1)
+mtext(bquote(p+q), side=1, line=1.5)
+dev.off()
+
+
 
 
