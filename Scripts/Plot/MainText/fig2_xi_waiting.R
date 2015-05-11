eeb97ca67cdc9b66e5e145ecf21741b673e2d57e
rbatt <battrd@gmail.com>
2015-05-11 15:50:34 -0400

    update figure dimensions for PNAS

--- Scripts/Plot/MainText/fig2_xi_waiting.R
+++ Scripts/Plot/MainText/fig2_xi_waiting.R
@@ -16,7 +16,7 @@ beanCol <- list(c(bFill[1]),
 				)
 
 # dev.new(width=3.5, height=6)
-png("/Users/Battrd/Documents/School&Work/WiscResearch/FatTails/Figures/MainText/Fig2_fatBeanXiWaiting.png", res=150, units="in", height=6, width=3.5)
+png("/Users/Battrd/Documents/School&Work/WiscResearch/FatTails/Figures/MainText/Fig2_fatBeanXiWaiting.png", res=150, units="in", height=5.862857, width=3.42)
 par(mfrow=c(2,1), mar=c(2,2.5,0.5,0.5), ps=10, cex=1, mgp=c(2, 0.4, 0), tcl=-0.3, family="Times")
 beanplot(sh_0~Type, data=data.fat, ylab="", yaxt="n", xaxt="n", border=bLine, col=beanCol, ll=0.01, beanlinewd=1.5)
 axis(side=2)
